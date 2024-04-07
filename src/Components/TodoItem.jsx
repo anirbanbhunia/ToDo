10872a19010f95e1c648e17282741631e084e736
anirbanbhunia <anirbanbhunia7@gmail.com>
2024-04-07 16:40:36 +0530

    second commit

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -17,7 +17,7 @@ const toggle = () => {
 
     return (
         <div
-            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
+            className={`flex items-center border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
                 todo.isChacked ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
             }`}
         >
